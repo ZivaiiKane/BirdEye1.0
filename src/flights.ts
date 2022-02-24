f4df0e5223912f5c135848274aecfb4b21ab9dc3
ZivaiiKane <zivaiikane@gmail.com>
2022-02-24 10:58:54 +0200

    feat: :sparkles: Implement APi using rxjs
    Implemeted API call using rxjs and typescript for stong types

---
+++ src/flights.ts
@@ -0,0 +1,46 @@
+import { fromFetch } from "rxjs/fetch";
+
+interface Flight {
+  icao24: string;
+  callsign: string;
+  origin_country: string;
+  time_position: number;
+  last_contact: number;
+  longitude: number;
+  latitude: number;
+  on_ground: boolean;
+}
+
+let flightsArray: Flight[] = [];
+
+let createObject = (data: any[]): Flight => {
+  const flight: Flight = {
+    icao24: data[0],
+    callsign: data[1],
+    origin_country: data[2],
+    time_position: data[3],
+    last_contact: data[4],
+    longitude: data[5],
+    latitude: data[6],
+    on_ground: data[7],
+  };
+
+  return flight;
+};
+
+const data$ = fromFetch("https://opensky-network.org/api/states/all", {
+  selector: (response) => response.json(),
+});
+
+data$.subscribe((result) => {
+  if (!result || !result.states) {
+    flightsArray = [];
+    return;
+  }
+  flightsArray = result.states
+    .slice(0, 100)
+    .map((data: (number | string | boolean)[]) => createObject(data));
+  console.log([flightsArray]);
+});
+
+export { flightsArray, Flight };
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "../dist/output.css";
 import { map } from "./map";
+import { flightsArray } from "./flights";
 
 map;
+flightsArray;
